eb22ca553ec50088cecbe483a6981cd15b579dc0
tswegner <tobiassimonwegner@gmail.com>
2015-07-22 08:42:08 +0200

    orders controller created, update for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -61,4 +61,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  # Routes for Orders
+  resources :orders, only: [:index, :show, :new, :create]  
 end
