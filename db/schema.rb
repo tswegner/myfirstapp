3a28da2a5d8b08b30b11afeaabf39f1256006aeb
tswegner <tobiassimonwegner@gmail.com>
2015-07-22 10:11:57 +0200

    created empty order database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721110807) do
+ActiveRecord::Schema.define(version: 20150722071116) do
+
+  create_table "orders", force: true do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.float   "total"
+  end
+
+  add_index "orders", ["product_id"], name: "index_orders_on_product_id"
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id"
 
   create_table "products", force: true do |t|
     t.string   "name"
