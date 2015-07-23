a2a41a8587f0c102204bb2a8a8d9f8fbf6b2ad57
tswegner <tobiassimonwegner@gmail.com>
2015-07-23 17:50:29 +0200

    navbar build in static_pages using routes, link to previouse page still failed

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#landing_page'
+  root 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
