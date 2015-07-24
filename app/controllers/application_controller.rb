5894edebe3eb46bacaa1aa72904e40422fc79345
tswegner <tobiassimonwegner@gmail.com>
2015-07-24 13:52:42 +0200

    back link in footer added, still no real redirect_to

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+#  Die Unteren defs hab ich mal angelegt um später im application.html.erb darauf zugreifen zu können. Nur leider bekomme ich das nicht hin.
+#  Ich denke ich muss noch ein wenig in die ganzen Zusammenhänge hier einarbeiten....   
+
+#  def back
+#    respond_to do |format|
+#      format.html {redirect_to :back}
+#      format.json {head :no_content}    
+#    end
+#  end
+  
+#  def back
+#    redirect_to :back    
+#  end
+
 end
