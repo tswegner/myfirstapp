ae7939829cc84c6e4f0d06adc5a963948dfc6d7e
tswegner <tobiassimonwegner@gmail.com>
2015-08-20 15:03:50 +0200

    stripe design ready

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807084502) do
+ActiveRecord::Schema.define(version: 20150820124344) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20150807084502) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "colour"
-    t.string   "price"
+    t.integer  "price",       limit: 255
   end
 
   create_table "users", force: true do |t|
