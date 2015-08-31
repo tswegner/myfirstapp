ab214159d1706602a6afb04afa90504db1bf6c48
tswegner <tobiassimonwegner@gmail.com>
2015-08-31 15:47:38 +0200

    ajax added to comment field, no reload any more

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,10 +5,13 @@ class CommentsController < ApplicationController
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
+    @comments = @product.comments.all.order("created_at DESC")
+    @comments = @comments.paginate(:page => params[:page], :per_page => 8)
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
+        format.js
       else
         format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
